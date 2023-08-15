bc1f8c74f7cb4e02de534872496542b7e4b56bc0
karsti11 <karlostipinovic11@gmail.com>
2023-08-15 17:37:44 +0200

    made main file into an introduction page

---
+++ src/streamlit_app/Introduction.py
@@ -0,0 +1,17 @@
+import streamlit as st
+from src.utils import get_project_root
+
+
+st.set_page_config(layout="wide")
+
+st.title(':coffee: Caffe bar sales, predictions and inventory analysis :wine_glass:')
+
+st.write('Hi! This app should help you have a better view of how your beverages retail business is doing.')
+
+st.subheader('Content')
+st.markdown('1. Main Overview')
+st.markdown('2. Model Evaluation')
+
+
+
+
