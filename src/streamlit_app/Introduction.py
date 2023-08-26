cf7699be2e1edfa0f21fd850ecc0e17907d78471
karsti11 <karlostipinovic11@gmail.com>
2023-08-26 20:57:34 +0200

    Added description to intro page

--- src/streamlit_app/Introduction.py
+++ src/streamlit_app/Introduction.py
@@ -5,8 +5,12 @@ from src.utils import get_project_root
 st.set_page_config(layout="wide")
 
 st.title(':coffee: Caffe bar sales, predictions and inventory analysis :wine_glass:')
-
-st.write('Hi! This app should help you have a better view of how your beverages retail business is doing.')
+st.write('')
+st.subheader('Description')
+st.write("""Hi! This app should help you have a better view of how your beverages retail business is doing.  
+    It consists of two pages, first one is Main Overview which serves as a summary of inventory and predictions  
+    for following dates and historical sales contribution by item. Second one is Model Evaluation, page for detailed analysis  
+    of model predictions.""")
 
 st.subheader('Content')
 st.markdown('1. Main Overview')
