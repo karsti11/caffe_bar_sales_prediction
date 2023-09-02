dd32037c3d7bbd5df0c542f2f4bb71f8a2664b7c
karsti11 <karlostipinovic11@gmail.com>
2023-09-02 16:24:28 +0200

    add Main Overview description

--- src/streamlit_app/Introduction.py
+++ src/streamlit_app/Introduction.py
@@ -11,11 +11,10 @@ st.write("""Hi! This app should help you have a better view of how your beverage
     It consists of two pages, first one is Main Overview which serves as a summary of inventory and predictions  
     for following dates and historical sales contribution by item. Second one is Model Evaluation, page for detailed analysis  
     of model predictions.""")
+st.write("""On Main Overview page, first section is KPIs overview, here we can see the overall direction of where our beverage business is going.    
+    There are metrics for sold quantity and value, error/accuracy and missing inventory situations.   
+    All are calculated so that year-to-date data from this year and year ago is taken into consideration.""")
 
 st.subheader('Content')
 st.markdown('1. Main Overview')
 st.markdown('2. Model Evaluation')
-
-
-
-
